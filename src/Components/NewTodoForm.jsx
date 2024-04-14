1f532ec663a6ecbd15969f59a2c469ef549440b5
mast8 <ronald.miranda.rodriguez@gmail.com>
2024-04-14 13:06:19 -0400

    validation css

--- src/Components/NewTodoForm.jsx
+++ src/Components/NewTodoForm.jsx
@@ -2,33 +2,37 @@ import { useState } from "react"
 
 export function NewTodoForm({ onSubmit }) {
   const [newItem, setNewItem] = useState("")
+  const [message, setMessage] = useState("")
 
   function handleSubmit(e) {
     e.preventDefault()
     if( newItem.trim() != "" ){
-
       onSubmit(newItem)
-      setNewItem("")
+      setMessage("")
     }
     else {
-      setNewItem("")
-      alert("Todo can not be empty")
+      setMessage("Todo can not be empty")
+      console.log(message)
+      //alert("Todo can not be empty")
     }
+    setNewItem("")
   }
 
   return (
     <form onSubmit={handleSubmit} >
       <div className="form-row">
-       
+        <span className="message" value={message}> {message} </span>
         <input className="todo-input"
           placeholder="Add new todo"
           value={newItem}
-          onChange={e => setNewItem(e.target.value)}
+          onChange={e => setNewItem(e.target.value) }
           type="text"
           id="item"
         />
         <button className="todo-btn">Add</button>
+        
       </div>
     </form>
+    
   )
 }
